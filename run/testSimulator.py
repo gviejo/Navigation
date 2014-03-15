02087aadc1da0dcabc26ec9d3738c88f9ad1d0ea
Guillaume <guillaume.viejo@gmail.com>
2014-03-15 19:19:54 +0100

    Working on simulator

---
+++ run/testSimulator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+testSimulator.py
+
+
+Copyright (c) 2013 Guillaume VIEJO. All rights reserved.
+"""
+
+from pyglet.gl import *
+from pyglet.window import key, mouse
+
+
+
+window = Window(width=800, height=600, caption='Pyglet', resizable=True)
